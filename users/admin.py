8c8e7ae73b0eecbb2214cac5fb6405fce16ad494
farahzadieng <farahzadiphy@gmail.com>
2024-04-10 22:44:30 +0330

    admin panel prep.

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .forms import SignUpForm
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    add_form = SignUpForm
+    # form = None
+    model = CustomUser
+    list_display = ('username', 'is_staff', 'is_active', )
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields' : ('phone_number', )}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields' : ('phone_number', )}),
+    )
+    
+    
+admin.site.register(CustomUser, CustomUserAdmin)
