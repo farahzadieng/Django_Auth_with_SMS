b5e8cfdb83e14793c438a762470769aa33867277
farahzadieng <farahzadiphy@gmail.com>
2024-04-10 22:21:08 +0330

    add CustomUser

--- users/models.py
+++ users/models.py
@@ -3,14 +3,25 @@ from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.base_user import BaseUserManager
 
 class UserManager(BaseUserManager):
-    def create_user(self, phone_number, code, **extra_fields):
+    def create_user(self, phone_number, **extra_fields):
         user = self.model(phone_number=phone_number, **extra_fields)
-        user.is_active = False  # Set user inactive until verified
+        user.is_active = True 
+        user.username = phone_number
         user.save(using=self._db)
         return user
+    def create_superuser(self, phone_number,password=None, **extra_fields):
+        user = self.model(phone_number=phone_number, **extra_fields)
+        user.set_password(password)
+        user.is_active = True  
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+        
 
 class CustomUser(AbstractUser):
-    phone_number = models.CharField(max_length=11, unique=True)  # Adjust length for your phone number format
+    phone_number = models.CharField(max_length=11, unique=True) 
+    password = models.CharField(max_length=128, blank=True) # For SuperUser Only
     REQUIRED_FIELDS = ['phone_number']
 
     objects = UserManager()
@@ -19,5 +30,7 @@ class CustomUser(AbstractUser):
         return self.phone_number
 
     def has_usable_password(self):
-        return False  # Override as we don't use a password field
-
+        if self.is_superuser:
+            return super().has_usable_password()
+        else:
+            return False
